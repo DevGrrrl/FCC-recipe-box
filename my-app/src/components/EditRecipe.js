38ec07e4a1ff842b2bd3fe33aec25326c6d15d56
Lex <alexisjaynedefoe@gmail.com>
2018-04-14 21:19:48 +0100

    basic styling of edit modal

--- my-app/src/components/EditRecipe.js
+++ my-app/src/components/EditRecipe.js
@@ -18,6 +18,38 @@ const Edit = styled.button`
 }
 `
 
+const Modal = styled.div`
+  /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+`
+
+const ModalContent = styled.div `
+  background-color: #fefefe;
+  display: block;
+  margin: 15% auto; /* 15% from the top and centered */
+  padding: 20px;
+  border: 1px solid #888;
+  background: #ffeded;
+  width: 80%; /* Could be more or less, depending on screen size */
+  `
+
+  const Label = styled.label`
+  display:block;
+ 
+
+`
+const TextArea = styled.textarea `
+  display:block;
+  padding: 2%;
+`
 const EditModal = props => {
 
   return (
@@ -26,8 +58,8 @@ const EditModal = props => {
         Edit
       </Edit>
       {props.editRecipeModalState ? (
-        <div id="myModal" className="modal">
-          <div className="modal-content">
+        <Modal id="myModal" className="modal">
+          <ModalContent className="modal-content">
             <span onClick = {props.toggleEditModal}  className="close">&times;</span>
             <form
               onSubmit={e => {
@@ -36,20 +68,20 @@ const EditModal = props => {
             >
               <fieldset>
                 <legend>Edit Recipe</legend>
-                <label htmlFor="enter-recipe-name">Recipe</label>
-                <textarea
+                <Label htmlFor="enter-recipe-name">Recipe</Label>
+                <TextArea
                   id="enter-recipe-name"
-                  cols="100"
+                  cols="35"
                   rows="1"
                   value={props.name}
                   placeholder={props.element.name}
                   onChange={props.handleRecipeName}
                 />
                 <p>
-                  <label htmlFor="enter-recipe-ingredients">Ingredients</label>
-                  <textarea
+                  <Label htmlFor="enter-recipe-ingredients">Ingredients</Label>
+                  <TextArea
                     id="enter-recipe-ingredients"
-                    cols="100"
+                    cols="35"
                     rows="5"
                     value={props.addIngredients}
                     onChange={props.handleIngredients}
@@ -59,8 +91,8 @@ const EditModal = props => {
                 <input type="submit" value="Submit" />
               </fieldset>
             </form>
-          </div>
-        </div>
+          </ModalContent>
+        </Modal>
       ) : (
      
         <div />
